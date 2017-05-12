7b7b770c2871d57d6911d013edb73a83db1067f9
Piard Brandon <pgm-27@live.fr>
2017-05-12 14:07:29 +0200

    Serveur de reception complet

--- src/serveurconcurrentUDP/Communication.java
+++ src/serveurconcurrentUDP/Communication.java
@@ -48,13 +48,41 @@ public class Communication implements Runnable {
         boolean communicationRunnig = true;
         System.out.println("Initialisation de la communication");
         sendingBuffer = "La connexion est établie".getBytes();
+        communicationPacket = new DatagramPacket(sendingBuffer,sendingBuffer.length);
+        try{
+            communicationSocket.send(communicationPacket);
+            System.out.println("La connexion est établie");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         
         while(!communicationSocket.isClosed()){
             try{
+                // On met en place se qu'il faut pour la récéption, puis on attends de recevoir des données
+                receptionBuffer = new byte [512];
+                communicationPacket = new DatagramPacket(receptionBuffer, receptionBuffer.length);
+                System.out.println("En attente de récéption");
+                communicationSocket.receive(communicationPacket);
                 
+                // Une fois reçu :
+                System.out.println("Thread : "+Thread.currentThread().getName());
+                System.out.println("Adresse du client : "+communicationPacket.getAddress()+ " | Port : "+communicationPacket.getPort() );
+                System.out.println("Message : "+byteToString(communicationPacket.getData()));
+                
+                
+            }catch(Exception e){
+                e.printStackTrace();
             }
         }
         
+    }
+    
+    public static String byteToString(byte[] bytes) {
+        String file_string = "";
+        for(int i = 0; i < bytes.length; i++) {
+            file_string += (char)bytes[i];
+        }
+        return file_string;
     }
     
 }
